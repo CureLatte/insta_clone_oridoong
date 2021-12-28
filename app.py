d23cec70c2f17763c7dc52df842d7f62998128a0
Ho Kim <hopao159@gmail.com>
2021-12-28 21:57:20 +0900

    추가 - MD 사용을 위해 작성한 기본 코드

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-client = MongoClient(
-    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/user", methods=["POST"])
+def user():
+    id_receive = request.form['id_give']
+    pwd_receive = request.form['pwd_give']
+
+    user_ifo = db.users.find_one({'name': 'bobby'})
+
+    return jsonify({'msg': '등록 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
