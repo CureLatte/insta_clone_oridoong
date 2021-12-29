5748f3eceae00f72d378d6d25bcf5951bcf6e910
김재성 <zhqmfkv@naver.com>
2021-12-29 12:41:17 +0900

    -체크아웃용 커밋

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
 client = MongoClient(
-    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
+    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile= ca)
+db = client.instaClone
 
 app = Flask(__name__)
 
 
+@app.route('/profile_main/')
+def road():
+    return render_template('profile_main.html')
 
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- static/javascript/profile_main_js.js
+++ static/javascript/profile_main_js.js
@@ -1,1 +1,8 @@
+$(document).ready(function() {
 
+        $("#avatar").attr("src", imgurl);
+    });
+
+function load_avatar{
+
+}
