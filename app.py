91bd5cd96b4214a44345f7c1bd046c4d7e2dd0e8
seongolee <dev@solee.net>
2021-12-29 17:15:17 +0900

    추가 - 텍스트박스 안에 이미지 처리 및 서버 api 따고, jsp 구축 시작.

--- app.py
+++ app.py
@@ -5,8 +5,42 @@ import certifi
 
 ca = certifi.where()
 client = MongoClient(
-    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
+    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    tlsCAFile=ca)
+db = client.instaClone
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('sign_up.html')
+
+
+@app.route("/sign_in", methods=["POST"])
+def user():
+    id_receive = request.form['id_give']
+    pwd_receive = request.form['pwd_give']
+
+    user_ifo = db.users.find_one({'user_id': 'id_receive'})
+    user_ifo = db.users.find_one({'user_pwd': 'pwd_receive'})
+
+    return jsonify({'msg': '등록 완료!'})
+
+
+@app.route("/sign_up/check_dup", methods=['POST'])
+def check_dup():
+    # ID 중복확인
+    user_id_receive = request.form['user_id_give']
+    check_id = db.user.find_one({'user_id': user_id_receive})
+
+    if check_id:
+        check_id = False
+    else:
+        check_id = True
+
+    return jsonify({'check_id': check_id})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5001, debug=True)
