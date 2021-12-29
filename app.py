437738d2ce02b8ba8ba13d1b9843575ef993d5b4
김재성 <zhqmfkv@naver.com>
2021-12-29 22:07:47 +0900

    -회의록 정리 및 app파일 확인

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ from flask import Flask, render_template, jsonify, request, session, redirect, u
 ca = certifi.where()
 
 client = MongoClient(
-    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile= ca)
+    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
 
 db = client.instaClone
 
 app = Flask(__name__)
+
+SECRET_KEY='TEST'
 
 
 @app.route('/login')
