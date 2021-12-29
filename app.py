af0355062bc2f96a2bea2637d63d5fd9a813195d
김재성 <zhqmfkv@naver.com>
2021-12-29 18:07:26 +0900

    수정 - 포트번호 그외 수정사항

--- app.py
+++ app.py
@@ -59,10 +59,6 @@ def move_addpage():
     return redirect(url_for('profile_main_page'))
 
 
-@app.route('/')
-def home():
-    return render_template('sign_up.html')
-
 @app.route('/edit_profile')
 def edit_profile():
     return render_template('edit_profile.html')
@@ -116,7 +112,6 @@ def user():
     return jsonify({'msg': '등록 완료!'})
 
 
-
 @app.route("/sign_up/check_dup", methods=['POST'])
 def check_dup():
     # ID 중복확인
@@ -130,7 +125,8 @@ def check_dup():
 
     return jsonify({'check_id': check_id})
 
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
 
 
