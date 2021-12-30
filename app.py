bb0e6f2394d7f4e49b967601214d2ab175f08783
seongolee <dev@solee.net>
2021-12-30 11:18:22 +0900

    추가 - input 텍스트 유효검사에 필요한 image file 2개, 기본적인 기능 완성 (재업)

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import hashlib
 import datetime
 import certifi
@@ -9,35 +8,29 @@ from flask import Flask, render_template, jsonify, request, session, redirect, u
 ca = certifi.where()
 
 client = MongoClient(
-    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
+    'mongodb+srv://seongo:123456789!@instagram.o4wki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    tlsCAFile=ca)
 
 db = client.instaClone
 
 app = Flask(__name__)
 
-SECRET_KEY='TEST'
+SECRET_KEY = 'TEST'
 
 
-@app.route('/login')
-def login():
+@app.route('/')  # token 획득을 확인
+def login_page():
     return render_template('login.html')
 
 
-@app.route('/')
-def home():
-    # 현재 이용자의 컴퓨터에 저장된 cookie 에서 mytoken 을 가져옵니다.
-    token_receive = request.cookies.get('mytoken')
-    try:
-        # 암호화되어있는 token의 값을 우리가 사용할 수 있도록 디코딩(암호화 풀기)해줍니다!
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', nickname=user_info["nick"])
-        # 만약 해당 token의 로그인 시간이 만료되었다면, 아래와 같은 코드를 실행합니다.
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        # 만약 해당 token이 올바르게 디코딩되지 않는다면, 아래와 같은 코드를 실행합니다.
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+@app.route('/login', methods=['POST'])
+def login_check():
+    user_id = request.form['id']
+    user_password = request.form['password']
+
+    user_check = list(db.user.find({'user_id': user_id}, {'_id': False}))
+    print(user_check)
+    return jsonify({'user': user_check})
 
 
 @app.route('/profile_main')
@@ -50,6 +43,20 @@ def load_info():
     user_id = request.form['user_id']
     user_info = list(db.user.find({'user_id': user_id}, {'_id': False}))
     return jsonify({'user_info': user_info})
+
+
+@app.route('/profile_main/move_edit')
+def move_edit_page():
+    print('hello!')
+
+    # 수정 필요!
+    return redirect(url_for('profile_main_page'))
+
+
+@app.route('/profile_main/move_add')
+def move_addpage():
+    # 수정 필요!
+    return redirect(url_for('profile_main_page'))
 
 
 @app.route('/edit_profile')
@@ -93,75 +100,15 @@ def edit_profile_post():
     return jsonify({'msg': 'DB등록 완료!'})
 
 
-
-    # 현재 이용자의 컴퓨터에 저장된 cookie 에서 mytoken 을 가져옵니다.
-    token_receive = request.cookies.get('mytoken')
-    try:
-        # 암호화되어있는 token의 값을 우리가 사용할 수 있도록 디코딩(암호화 풀기)해줍니다!
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', nickname=user_info["nick"])
-        # 만약 해당 token의 로그인 시간이 만료되었다면, 아래와 같은 코드를 실행합니다.
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        # 만약 해당 token이 올바르게 디코딩되지 않는다면, 아래와 같은 코드를 실행합니다.
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
-@app.route('/api/login', methods=['POST'])
-def api_login():
+@app.route("/sign_in", methods=["POST"])
+def user():
     id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
+    pwd_receive = request.form['pwd_give']
 
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    user_ifo = db.users.find_one({'user_id': 'id_receive'})
+    user_ifo = db.users.find_one({'user_pwd': 'pwd_receive'})
 
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=암호화 풀기)해서 payload 값을 볼 수 있습니다.
-        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
-        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
-        payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=300)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-
-@app.route('/api/nick', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
-
-    # try / catch 문?
-    # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
-
-    try:
-        # token을 시크릿키로 디코딩합니다.
-        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        print(payload)
-
-        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
-        # 여기에선 그 예로 닉네임을 보내주겠습니다.
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-
+    return jsonify({'msg': '등록 완료!'})
 
 
 @app.route('/sign_up/check_dup', methods=['POST'])
@@ -188,3 +135,4 @@ def sign_up():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
+
