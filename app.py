95c341a4d8cd71ae4e49d8dedd805fded39d5a40
김재성 <zhqmfkv@naver.com>
2021-12-30 14:35:09 +0900

    심일님 - 수동 머지작업

--- app.py
+++ app.py
@@ -126,6 +126,23 @@ def check_dup():
     return jsonify({'check_id': check_id})
 
 
+@app.route("/writing_new")
+def writing():
+    return render_template('writing_new.html')
+
+
+@app.route("/writing_new", methods=["POST"])
+def new_writing():
+    text_receive = request.form['text_receive']
+
+    doc = {
+        "desc": text_receive,
+    }
+    db.post_content.insert_one(doc)
+
+    return jsonify({'msg': '등록완료'})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
 
